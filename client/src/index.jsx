cda4fd576059e20485131e1248b74c26ca2c0626
Harrison Wu <harrisonywu@gmail.com>
2019-04-27 15:07:10 -0700

    Refactor file and variable names to use underscores

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import Overview from './components/Overview.jsx';
+// import Overview from './components/Overview.jsx';
 
-window.Overview = Overview;
+// window.Overview = Overview;
 window.Sidebar = App;
