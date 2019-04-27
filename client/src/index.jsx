0991c9e21fee5dfc08adaeb8bd3c9102baf315d3
Harrison Wu <harrisonywu@gmail.com>
2019-04-27 15:09:24 -0700

    Remove obsolete comments

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-// import Overview from './components/Overview.jsx';
 
-// window.Overview = Overview;
 window.Sidebar = App;
