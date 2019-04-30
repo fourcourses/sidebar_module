6fa5043d90b82b28526302255082ec38aa48c2c8
Harrison Wu <harrisonywu@gmail.com>
2019-04-30 13:25:48 -0700

    Implement Redis Cache

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cassandra = require('cassandra-driver');
+const redis = require('redis');
+const redisClient = redis.createClient();
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1', keyspace: 'Restaurants' });
+
 
 const path = require('path');
 const cors = require('cors');
@@ -14,17 +17,38 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/restaurants/:id', express.static(path.join(__dirname, '/../client/dist')));
 
-
+//Fix this to use redis cache
 const findById = (req, res) => {
   const query =`SELECT * FROM "RestaurantListing" WHERE restaurant_id = ?`;
   const params = req.params.id;
   client.execute(query, [ params ], { prepare: true})
-    .then(result => res.send(result.rows[0]))
-    .catch(err => console.log('Received an error while querying: ', err));
+    .then(result => {
+      result = result.rows[0]
+      resultStringified = JSON.stringify(result);
+      redisClient.setex(params, 3600, resultStringified);
+      res.status(200);
+      res.send(result);
+      
+    })
+    .catch(err => {
+      res.status(404);
+      res.send('Error while querying: ', err)
+    });
+}
+
+const getCache = (req, res) => {
+  let id = req.params.id;
+  redisClient.get(id, (err, result) => {
+    if (result) {
+      res.send(result);
+    } else {
+      findById(req, res);
+    }
+  })
 }
 
 // For Cassandra
-app.get('/api/restaurants/:id/info', findById);
+app.get('/api/restaurants/:id/info', getCache);
 
 const server = app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
