be952832854236dd61d86b916293a5804a76357e
Harrison Wu <harrisonywu@gmail.com>
2019-04-30 15:00:18 -0700

    Stress Test: Last 100 being hit 50% of the time

--- server/k6StressTesting.js
+++ server/k6StressTesting.js
@@ -2,17 +2,22 @@ import http from "k6/http";
 import { check } from "k6";
 
 export let options = {
-  vus: 100,
-  duration: "3000s",
-  rps: 1000,
+  vus: 150,
+  duration: "120s",
   noVUConnectionReuse: true,
   noConnectionReuse: true,
  };
 
+
+let last100 = Math.floor(Math.random() * 100) + 9999901
 let randomID = Math.floor(Math.random() * 10000000) + 1;
 
+let idPicker = ((randomValue) => {
+  return randomValue > 0.5 ? randomID : last100;
+})(Math.random())
+
 export default function() {
-  let res = http.get(`http://localhost:3003/api/restaurants/${randomID}/info`);
+  let res = http.get(`http://localhost:3003/api/restaurants/${idPicker}/info`);
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 200
